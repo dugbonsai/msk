c37f6725516a20477e602e6db28b476a7e55d8d2
dugbonsai <dwbonser@mac.com>
2025-06-01 11:57:52 -0500

    Add files via upload

--- createTruststore.sh
+++ createTruststore.sh
@@ -7,7 +7,7 @@ awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_aft
 
 for CERT in rds-ca-*; do
   alias=$(openssl x509 -noout -text -in $CERT | perl -ne 'next unless /Subject:/; s/.*(CN=|CN = )//; print')
-  echo "Importing $alias"
+  echo "Importing $alias" >> createTruststore.log
   keytool -import -file ${CERT} -alias "${alias}" -storepass ${storepassword} -keystore ${truststore} -noprompt
   rm $CERT
 done
@@ -17,5 +17,5 @@ echo "Trust store content is: "
 keytool -list -v -keystore "$truststore" -storepass ${storepassword} | grep Alias | cut -d " " -f3- | while read alias
 do
    expiry=`keytool -list -v -keystore "$truststore" -storepass ${storepassword} -alias "${alias}" | grep Valid | perl -ne 'if(/until: (.*?)\n/) { print "$1\n"; }'`
-   echo " Certificate ${alias} expires in '$expiry'"
+   echo " Certificate ${alias} expires in '$expiry'" >> createTruststore.log
 done
